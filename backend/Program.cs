21b16cf7b09902ebaa4d3f652d7cafcea37bb70d
ibBenwah <garret.c.benoit@gmail.com>
2026-02-09 20:59:32 -0500

    Connected frontend to backend with CORS configuration

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,7 +6,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [forecasts, setForecasts] = useState([]);
+  const [loading, setLoading] =useState(true);
+
+  useEffect(() => {
+    fetch('https://localhost:7157/weatherforecast')
+    .then(response => response.json())
+    .then(data => {
+      setForecasts(data);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.log('Error fetching data: ', error);
+      setLoading(false);
+    });
+  }, []);
+
+  return(
+    <div style={{textAlign: 'center', padding: '50px', fontFamily: 'Arial, sans-serif' }}>
+      <h1> Hockey Stats Hub</h1>
+      <p>Your source for in-depth hockey stats and news</p>
+
+      <h2>API Connection test</h2>
+      {loading ? (
+        <p>Loading data from backend...</p>
+      ) : forecasts.length > 0 ? (
+        <p>Connected! Got {forecasts.length} records from the backend.</p>
+      ) : (
+        <p>Cound not connect to the backend.</p>
+      )}
+      </div>
   );
 }
 
